aa0c1b9fe9259bfc263df774b83eefe520d8699d
Ricardo Perez <rxperez12@gmail.com>
2024-05-21 16:54:36 -0700

    mocked get random discount function

--- costs.test.js
+++ costs.test.js
@@ -1,14 +1,25 @@
 import { test, beforeAll, afterAll, expect, vi } from "vitest";
 import { getCost } from "./costs.js";
+import * as discounts from "./discounts.js";
+
+beforeAll(function () {
+  vi.spyOn(discounts, "getRandomDiscount");
+});
+
+afterAll(function () {
+  vi.resetAllMocks();
+});
 
 test("cheap product", function () {
   // cheap products have IDs between 1000-1999, and cost $100 * discount
+  discounts.getRandomDiscount.mockReturnValue(.1);
   const cost = getCost(1200);
   expect(cost).toBe("90.00");
 });
 
 test("expensive product", function () {
   // expensive products have IDs greater than 2000, and cost $200 * discount
+  discounts.getRandomDiscount.mockReturnValue(.1);
   const cost = getCost(2500);
   expect(cost).toBe("180.00");
 });
