6e87f948a62881641c29148e7f023af44da4f038
Ricardo Perez <rxperez12@gmail.com>
2024-05-21 17:12:37 -0700

    added changes to ship test -WIP

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,8 +1,19 @@
 import { test, expect, beforeAll, afterAll, afterEach } from "vitest";
 import { shipViaShipIt, SHIPIT_SHIP_URL } from "./shipItApi.js";
+import { mock, mockServer } from "./apiMock.js";
+
+beforeAll(function () { mockServer.listen(); });
+afterEach(function () { mockServer.resetHandlers(); });
+afterAll(function () { mockServer.close(); });
 
 test("shipProduct", async function () {
-  const trackingId = await shipViaShipIt({
+  const orderId = '123';
+  const trackingId = 789;
+  const cost = 90;
+
+  mock('post', SHIPIT_SHIP_URL, '/orders/123/ship', { orderId, trackingId, cost });
+
+  const response = await shipViaShipIt({
     orderId: 123,
     productId: 1000,
     name: "Test Tester",
@@ -10,5 +21,6 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(trackingId).toEqual(789);
+
+  expect(response).toEqual(789);
 });
